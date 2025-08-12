dfec391c29caaf5c8b8911db50ea10cdca5b9e5a
Dushmantha Ekanayake <dushmantha.e@gmail.com>
2025-08-12 11:54:34 -0500

    added code for a temp directory in setUp()

--- src/test/java/com/pkg/base/BaseTest.java
+++ src/test/java/com/pkg/base/BaseTest.java
@@ -1,7 +1,11 @@
 package com.pkg.base;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,8 +14,11 @@ public class BaseTest {
 	protected WebDriver wdriver;
 	
 	@BeforeMethod
-	public void setUp(){
-		wdriver = new ChromeDriver();
+	public void setUp() throws IOException{
+		ChromeOptions options = new ChromeOptions();
+	    String tempDir = Files.createTempDirectory("chrome-user-data").toString();
+	    options.addArguments("--user-data-dir=" + tempDir);
+		wdriver = new ChromeDriver(options);
 		wdriver.manage().window().maximize();
 		wdriver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
 	}
